ea64d1f35b6f28d73168663f3dfa985753908471
Alfredo López Moltó <xgalen@gmail.com>
2016-02-03 22:07:31 +0100

    Get the user token

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var express = require('express'),
-    app = module.exports = express();
+    app = module.exports = express(),
+    userId = 'sewriiojclvkslwr',
+    request = require('basic-request');
+global.token = global.token || getToken(userId);
+
+function getToken(id) {
+    request.get('http://billing.mediasmart.mobi/api/auth?user=' + id, function (error, body) {
+        if (error) {
+            console.error('Could not access billing auth: %s', error);
+            return;
+        }
+	token = JSON.parse(body).token;
+    });
+}
